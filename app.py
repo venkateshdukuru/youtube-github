18be78d14a0b9e46ca9702b154e594ef379da8e5
Dukuru Venkatesh <venkateshdukuru2266@gmail.com>
2025-11-21 05:32:02 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-"hello" 
+
+def is_prime(n):
+  """Checks if a number is prime."""
+  if n <= 1:
+    return False
+  for i in range(2, int(n**0.5) + 1):
+    if n % i == 0:
+      return False
+  return True
+
+# Get input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is prime
+if is_prime(num):
+  print(num, "is a prime number")
+else:
+  print(num, "is not a prime number")
